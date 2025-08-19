f5e2d297e4a017e148bf0e7ac9b6a81aafcc4023
HomeFinder <167259553+Zali0@users.noreply.github.com>
2025-08-19 07:59:53 +0000

    b

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 import bcyrpt from 'bcryptjs'
 
-export default function Admin() {
+export default async function Admin() {
   const [users, setUsers] = useState([]);
   const [properties, setProperties] = useState([]);
 
@@ -277,7 +277,7 @@ export default function Admin() {
             <input placeholder="Name" type="text" name="name" value={UserFormData.name} onChange={handleUserChange} />
             <input placeholder="Email" type="email" name="email" value={UserFormData.email} onChange={handleUserChange} />
             
-            <input placeholder="Password" type="text" name="password" value={isEditingUser ?  bcyrpt.hash(UserFormData.password, 10) : UserFormData.password} onChange={handleUserChange} /> 
+            <input placeholder="Password" type="text" name="password" value={isEditingUser ?  await bcyrpt.hash(UserFormData.password, 10) : UserFormData.password} onChange={handleUserChange} /> 
             {/* disabled={isEditingUser} /> */}
 
 
